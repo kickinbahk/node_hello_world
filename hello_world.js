64fd7d8176048a42184ec6b1412df50600fe63d9
kickinbahk <kickinbahk@gmail.com>
2016-01-30 17:11:05 -0800

    Add http module and make simple web server"

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,8 @@
-console.log("Hello, World");
+var http = require('http');
+
+http.createServer(function(request, response){
+    response.writeHead({
+        "Content-Type": "text/plain"
+    });
+    response.end("Hello, World");
+}).listen(3000);
